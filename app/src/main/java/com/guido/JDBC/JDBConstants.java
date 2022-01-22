074b856b4df3ab2d7e2a7c2a35032de7e31a9277
tortuga <daniel2001faria@gmail.com>
2022-01-22 23:05:21 +0000

    changes password

--- app/src/main/java/com/guido/JDBC/JDBConstants.java
+++ app/src/main/java/com/guido/JDBC/JDBConstants.java
@@ -4,7 +4,7 @@ public class JDBConstants {
     public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
     public static final String URL = "jdbc:mysql://guido-db.mysql.database.azure.com:3306/guidoo?useSSL=true";
     public static final String USERNAME = "adminguido";
-    public static final String PASSWORD = "Li4guido";
+    public static final String PASSWORD = "123SQLserver123";
     public static final int NAME_SIZE = 32;
     public static final int CATEGORY_NAME_SIZE = 64;
     public static final int LOCATION_NAME_SIZE = 64;
