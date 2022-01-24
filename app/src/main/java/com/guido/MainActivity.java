80781d99f579ecdc701e0bcc394b21c2401c5677
Gonçalo Pinto <blanc@blanc-mac.home>
2022-01-24 12:09:22 +0000

    imports

--- app/src/main/java/com/guido/MainActivity.java
+++ app/src/main/java/com/guido/MainActivity.java
@@ -2,20 +2,13 @@ package com.guido;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Parcelable;
-import android.os.StrictMode;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.view.View;
 
 import com.guido.Controller.Controller;
 import com.guido.Controller.IController;
-import com.guido.JDBC.JDBCQueries;
 
 import java.io.Serializable;
-import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
     private IController controller;
